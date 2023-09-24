fe9a5a8cb9422c043a89e6e18223dee0c339532e
Fly <2372563520@qq.com>
2023-09-24 15:46:06 +0800

    update

--- bit/1.addWithoutPlus.cpp
+++ bit/1.addWithoutPlus.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+
+/**
+ * 位运算中，两数进行异或运算可以提供两数加和之后二进制非进位信息
+ * 两数进行与运算可以提供两数加和后的二进制进位信息
+ * 将两数的与运算的结果循环左移一位，在下一轮循环中继续将进位结果和非进位结果求和，
+ * 重复此过程，直到不再产生进位为止
+*/
 int add(int a,int b){
     while(b!=0){
         unsigned int carry=(unsigned int)(a&b)<<1;    //进位部分
