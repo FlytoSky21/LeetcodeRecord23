5fa3655069544e3503a4b643778f888c83108de1
Fly <2372563520@qq.com>
2023-10-12 15:23:02 +0800

    update

--- stack/2.decodeString.cpp
+++ stack/2.decodeString.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+#include<sstream>
 using namespace std;
 
 string decodeString(string s){
@@ -32,8 +33,38 @@ string decodeString(string s){
     return str;
 }
 
+string decodeString1(string s){
+    stack<pair<string,int>> stk;
+    int num=0;
+    string str="";
+    for(char ch:s){
+        if(isdigit(ch)){
+            num=num*10+int(ch-'0');
+        }else if(ch=='['){
+            stk.push({str,num});
+            str="";
+            num=0;
+        }else if(isalpha(ch)){
+            str+=ch;
+        }else if(ch==']'){
+            int times=stk.top().second;
+            string pre=stk.top().first;
+            stk.pop();
+            // string temp="";
+            // while(times--){
+            //     temp+=str;
+            // }
+            // str=pre+temp;
+            stringstream ss;
+            while(times--)  ss<<str;
+            str=pre+ss.str();
+        }
+    }
+    return str;
+}
+
 int main(){
     string s = "2[abc]3[cd]ef";
-    cout<<decodeString(s)<<endl;
+    cout<<decodeString1(s)<<endl;
     return 0;
 }
